9f9e793cbc14fdfb348aa65494d47b5c8dfac219
comicsansMC <shanedyke12@gmail.com>
2023-05-15 09:26:24 -0700

    Work on Ship.java not finished

--- Ship.java
+++ Ship.java
@@ -21,12 +21,13 @@ public class Ship extends MovingThing
 
 	public Ship(int x, int y)
 	{
-	   //add code here
+	   super(x,y);
 	}
 
 	public Ship(int x, int y, int s)
 	{
-	   //add code here
+	   super(x,y);
+	   speed = s;
 	}
 
 	public Ship(int x, int y, int w, int h, int s)
@@ -40,19 +41,19 @@ public class Ship extends MovingThing
 		}
 		catch(Exception e)
 		{
-			//feel free to do something here
+			System.out.print("Something donked up with the ship image");
 		}
 	}
 
 
 	public void setSpeed(int s)
 	{
-	   //add more code
+	   speed = s;
 	}
 
 	public int getSpeed()
 	{
-	   return 0;
+	   return speed;
 	}
 
 	public void move(String direction)
